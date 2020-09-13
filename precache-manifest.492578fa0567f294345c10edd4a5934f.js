2c24b1685240c2fc424a4598ed816eb161c00df5
James PC <james.tran1191@gmail.com>
2020-09-13 16:31:39 +1000

    Updates

---
+++ precache-manifest.492578fa0567f294345c10edd4a5934f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad59c2b71d74048352598b4951c42ae4",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "c9547f0f35d8ae32587c",
+    "url": "/simple-todo-app/static/css/main.247aee79.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9547f0f35d8ae32587c",
+    "url": "/simple-todo-app/static/js/main.a6d5ffb4.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
